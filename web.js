47af3f80c47ade1b57b633694287ff8be8419883
Kent Fischer <kent_fish@hotmail.com>
2013-07-10 04:07:05 +0000

    Added web.js buffer

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var express = require('express');
 
-//var buffer = new Buffer;
-//buffer = fs.readFileSync(index.html);
+var buffer = new Buffer;
+buffer = fs.readFileSync(index.html);
 
 //buffer.write(buf.tostring(buffer);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
