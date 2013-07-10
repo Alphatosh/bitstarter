b10670599089f7d84348ddf628619549c28a23a7
Kent Fischer <kent_fish@hotmail.com>
2013-07-10 04:18:43 +0000

    Added web.js buffer

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
-var fs = require ('fs')
-var buffer = new Buffer;
-buffer = fs.readFileSync('index.html', 'utf8');
+//var fs = require ('fs')
+//var buffer = new Buffer;
+//buffer = fs.readFileSync('index.html', 'utf8');
 
 //buffer.write(buf.tostring(buffer);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+    var fs = require('fs');
+    var buf = new buffer(fs.readFileSync('index.html','utf-8'));
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
